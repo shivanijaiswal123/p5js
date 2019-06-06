495c68c82bda093ddf6a2614198824fad0c1d9fc
shivanijaiswal123 <shivanij18@navgurukul.org>
2019-06-06 11:11:02 +0530

    use of noStroke fun.

---
+++ sketch2.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background(200);
+}
+
+function draw() {
+  noStroke();
+  fill(250,200,200)
+  ellipse(mouseX,mouseY,100,100,100);
+  
+  fill(250,200,200)
+  rect(320,300,70,100);
+}
