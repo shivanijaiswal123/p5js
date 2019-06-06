1ad1c0357431d12d4480fb9baffa63ad6247f50f
shivanijaiswal123 <shivanij18@navgurukul.org>
2019-06-06 10:56:22 +0530

    background in setup fun.

---
+++ sketch1.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+  background(200);
+}
+
+function draw() {
+
+  fill(250,200,200)
+  ellipse(mouseX,100,100,100);
+  
+  fill(250,200,200)
+  rect(320,300,70,100);
+}
